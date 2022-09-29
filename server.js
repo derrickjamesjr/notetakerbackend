52189bdd5d6a0667f99755c9929d8f140742bb75
Derrick James <ballyouth@gmail.com>
2022-09-29 19:00:52 -0400

    posted homepage

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import express package
+const express = require("express");
+
+// Require the JSON file and assign it to a variable called `termData`
+const termData = require("./db/db.json");
+const PORT = 3001;
+
+// Initialize our app variable by setting it to the value of express()
+const app = express();
+
+// Add a static route for index.html
+app.get("/", (req, res) => {
+  // `res.sendFile` is Express' way of sending a file
+  // `__dirname` is a variable that always returns the directory that your server is running in
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// res.json() allows us to return JSON instead of a buffer, string, or static file
+app.get("/api", (req, res) => res.json(termData));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
