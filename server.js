21b93a9185729b0780222ea226b52f74c19c7dfd
Derrick James <ballyouth@gmail.com>
2022-09-29 19:06:03 -0400

    posted notespaga and linked it

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 // Require the JSON file and assign it to a variable called `termData`
-const termData = require("./db/db.json");
+const dbData = require("./db/db.json");
 const PORT = 3001;
 
 // Initialize our app variable by setting it to the value of express()
@@ -14,9 +14,12 @@ app.get("/", (req, res) => {
   // `__dirname` is a variable that always returns the directory that your server is running in
   res.sendFile(__dirname + "/public/index.html");
 });
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+});
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get("/api", (req, res) => res.json(termData));
+app.get("/api", (req, res) => res.json(dbData));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
