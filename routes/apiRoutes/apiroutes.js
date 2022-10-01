d92a1c6655aa90e77f1df65d7107f47905f7a445
Derrick James <ballyouth@gmail.com>
2022-10-01 14:01:35 -0400

    point to mess with

--- routes/apiRoutes/apiroutes.js
+++ routes/apiRoutes/apiroutes.js
@@ -1,8 +1,21 @@
 const express = require ('express');
 const router = express.Router();
 const dbData = require("../../db/db.json");
+const fs = require('fs');
 
 router.get("/notes", (req, res) => res.json(dbData));
+
+router.post("/notes" , (req, res) => {
+    const notebdy = req.body;
+    console.log(req.body);
+
+    fs.readFile("db/db.json", 'utf8', (err, data) =>  {
+        if (err) throw err;
+        console.log(data);
+    console.log(JSON.parse(data));})
+    
+
+});
 
 
 module.exports = router;
