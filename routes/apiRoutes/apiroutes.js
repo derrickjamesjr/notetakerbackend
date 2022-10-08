de662d925e313b8d6cdf9623c7d5434f50c59dbd
Derrick James <ballyouth@gmail.com>
2022-10-08 14:00:08 -0400

    finish last bit of challenge

--- routes/apiRoutes/apiroutes.js
+++ routes/apiRoutes/apiroutes.js
@@ -1,31 +1,52 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
 const dbData = require("../../db/db.json");
-const fs = require('fs');
+const fs = require("fs");
 
 router.get("/notes", (req, res) => res.json(dbData));
 
-router.post("/notes" , (req, res) => {
-    const notebdy = req.body;
-    console.log(req.body);
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
+router.post("/notes", (req, res) => {
+  const notebdy = req.body;
+  // const newdb = [];
+  // console.log(req.body);
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    // console.log(data);
+    console.log("incoming from db.json", JSON.parse(data));
+    // newdb.push(JSON.parse(data));
+    const obj = JSON.parse(data);
+    console.log(obj);
+    obj.push(notebdy);
+    obj.forEach((dbData, i) => {
+      dbData.id = i + 1;
+    });
+    // write(obj);
+
+    // console.log(newdb);})
+  });
+  function write(data) {
+    fs.writeFile(
+      "db/db.json",
+      JSON.stringify(data, "/t"),
+      "utf8",
+      (err, data) => {
         if (err) throw err;
         console.log(data);
-        // console.log(JSON.parse(data));
-        dbData.push(notebdy);
-        console.log(JSON.parse(data));})
-        fs.writeFile('db/db.json', JSON.stringify(data, '/t'), 'utf8', (err, data) => {
-            if (err) throw err;
-            console.log(data);
-        })
-    })
+      }
+    );
+  }
+  function render(data) {
 
-router.get("/notes/:id", (req, res) => {
-    
-})
+  }
+});
 
-    // fs.writeFile('db/db.json', JSON.stringify(dbData), (err, data) => {
-    //     if (err) throw err;
-    //     console.log(data);
+// router.get("/notes/:id", (req, res) => {
+//     const id = req.params.id;
+
+// })
+
+// fs.writeFile('db/db.json', JSON.stringify(dbData), (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
 
 module.exports = router;
